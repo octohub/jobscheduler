722298d49f066db64ea9c84f3697420b0de3dd27
Richard Lucas <richard.lucas@raizlabs.com>
2018-01-09 19:40:26 -0800

    Comment out schedule on reboot.

--- app/src/main/java/com/example/richard/jobscheduler/MyStartServiceReceiver.java
+++ app/src/main/java/com/example/richard/jobscheduler/MyStartServiceReceiver.java
@@ -7,6 +7,11 @@ import android.content.Intent;
 public class MyStartServiceReceiver extends BroadcastReceiver {
 
   @Override public void onReceive(Context context, Intent intent) {
-    Util.scheduleJob(context);
+    /*
+    Careful, commenting this line out will allow the job to keep scheduling
+    itself even after reboot. This can cause a situation where you must use ADB
+    to uninstall the app in order to make your device usable again.
+     */
+    //Util.scheduleJob(context);
   }
 }
